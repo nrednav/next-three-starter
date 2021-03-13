0f6018dbc4956a4e0ab22030f199b06d91dad90c
nrednav <nrednav@gmail.com>
2021-03-13 19:27:48 +0400

    Setup project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next Starter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Next Starter</h1>
+    </div>
+  );
+}
